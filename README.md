64d4836a7aaacebb4d8ba575d263972613a3341b
Luz T <luztalaveramartinez@hotmail.com>
2022-11-03 10:26:43 -0300

    Login realizado con PHP  y MVC

---
+++ README.md
@@ -0,0 +1,1 @@
+# Login--hecho-con-MVC
---
+++ view/user/index.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+// Validamos que exista una session y ademas que el cargo que exista sea igual a 1 (Administrador)
+if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 2) {
+  header('location: ../../index.php');
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>User</title>
+</head>
+
+<body>
+  <!-- ucfirst convierte la primera letra en mayusculas de una cadena -->
+  Hola <?php echo ucfirst($_SESSION['nombre']); ?>, que bueno tenerte de nuevo con nosotros!<br><br>
+              
+
+  <a href="../../controller/cerrarSesion.php">
+    <button class="boton-cerrar-sesion" type="button" name="button">Cerrar sesion</button>
+  </a>
+</body>
+
+</html>
